48829add08fee754685f1e130432167a2b4ee79a
dor917 <donha917@naver.com>
2023-07-11 20:46:52 +0900

    스프링 데이터 JPA가 제공하는 Querydsl 기능

---
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package study.querydsl.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.entity.Member;
+
+import java.util.List;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
+    List<Member> findByUsername(String username);
+
+    Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable);
+
+    Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable);
+}
